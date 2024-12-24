ece0f93c220ad0caf5eceb463730adb96c1dfd06
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2024-12-24 09:35:46 +0530

    Add tabs layout

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#11DD66',
+        headerStyle: {
+          backgroundColor: '#1C1E27',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#F0F0F0',
+        tabBarStyle: {
+          backgroundColor: '#1C1E27',
+        },
+      }}
+    >
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='about'
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
+import { registerRootComponent } from 'expo';
+import { Link } from 'expo-router';
 import { Text, View, StyleSheet } from 'react-native';
 
-export default function Index() {
+const Index = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World 👋🏼</Text>
+      <Link href='/about' style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -14,8 +19,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#1C1E27',
+    gap: 16,
   },
   text: {
     color: '#F0F0F0',
   },
+  button: {
+    fontSize: 16,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
 });
+
+registerRootComponent(Index);
+
+export default Index;
